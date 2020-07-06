afcae526da8850b5e0d7cbf7b484d8a0f8a4ee0d
xiaoye <152156049@qq.com>
2020-07-06 16:27:18 +0800

    更新了我的简历首页

---
+++ js/custom.js
@@ -0,0 +1,34 @@
+$(function() {
+    $('.navbar-right li').on('click', function() {
+        var index = $(this).index() + 1
+        $('html,body').animate({
+            scrollTop: $('.y_Weiz').eq(index).offset().top
+        }, 500);
+
+    })
+    $('.back-to-top a').on('click', function() {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 500);
+
+    })
+});
+jQuery(document).ready(function($) {
+    var $timeline_block = $('.cd-timeline-block');
+    $timeline_block.each(function() {
+        if ($(this).offset().top > $(window).scrollTop() + $(window).height() * 0.75) {
+            $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+        }
+    });
+    $(window).on('scroll', function() {
+        $timeline_block.each(function() {
+            if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden')) {
+                $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+            }
+        });
+    });
+});
+
+$(".navbar-collapse").css({
+    maxHeight: $(window).height() - $(".navbar-header").height() + "px"
+});
