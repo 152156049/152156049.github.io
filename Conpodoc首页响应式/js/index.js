de9f16df319ab847604c948b3e9be72110bb8e8c
xiaoye <152156049@qq.com>
2020-07-07 20:13:56 +0800

    增加了个人信息内容

---
+++ Conpodoc首页响应式/js/index.js
@@ -0,0 +1,63 @@
+$(function() {
+    // 页面标语初始化显示
+    $(".y_biaoyu_dis").animate({
+            paddingTop: 0,
+            opacity: 1,
+        },
+        1000
+    );
+
+    // 滚动显示
+    Rollingtransla()
+
+    function Rollingtransla() {
+        var mbiao = $(".y_tezhen_top").offset().top;
+
+        $(document).on("scroll ", function() {
+
+            if ($(this).scrollTop() >= mbiao - 400) {
+                $(".y_tezhen_top li").stop(true).animate({
+                        opacity: 1,
+                        top: 0,
+                    },
+                    500
+                );
+                $(".y_tezhen_botton li").stop(true).animate({
+                        opacity: 1,
+                        bottom: 0,
+                    },
+                    500
+                );
+            } else if ($(this).scrollTop() <= mbiao) {
+                $(".y_tezhen_top li").css({
+                    opacity: 0,
+                    top: -20 + "px",
+                });
+                $(".y_tezhen_botton li").css({
+                    opacity: 0,
+                    bottom: -20 + "px",
+                });
+            }
+
+            $(".animated").each(function(index, element) {
+                if ($(element).offset().top <= $(document).scrollTop() + window.innerHeight) {
+                    $(element).stop(true).animate({
+                            opacity: 1,
+                            top: 0,
+                        },
+                        500
+                    );
+                } else {
+                    $(element).stop(true).animate({
+                            opacity: 0,
+                            top: -10 + "px",
+                        },
+                        500
+                    );
+                }
+            });
+        });
+    }
+
+
+});
