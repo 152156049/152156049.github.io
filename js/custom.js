94f623c66114e72b53c060238fcd6d7f8870d63b
xiaoye <152156049@qq.com>
2020-07-09 20:48:35 +0800

    简历增加了折叠卡,作品加了2D划入效果

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,12 @@
 jQuery(document).ready(function($) {
+    // 初始化隐藏
     var $timeline_block = $('.cd-timeline-block');
     $timeline_block.each(function() {
         if ($(this).offset().top > $(window).scrollTop() + $(window).height() * 0.75) {
             $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
         }
     });
+    // 滚动到指定位置添加动画效果
     $(window).on('scroll', function() {
         $timeline_block.each(function() {
             if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden')) {
@@ -27,17 +29,25 @@ $(function() {
         }, 500);
 
     })
-});
-$(".navbar-collapse").css({
-    maxHeight: $(window).height() - $(".navbar-header").height() + "px"
-});
 
-$('#open').on("click", function(e) {
-    $('#main-menu').stop().slideToggle()
-})
-$(document).on("click", function(e) {
-    if ($(e.target).closest("#main-menu").length == 0 && $(e.target).closest("#open").length == 0) {
-        $('#main-menu').stop().slideUp()
+    $(".navbar-collapse").css({
+        maxHeight: $(window).height() - $(".navbar-header").height() + "px"
+    });
 
-    }
-})
+    $('#open').on("click", function(e) {
+        $('#main-menu').stop().slideToggle()
+    })
+    $(document).on("click", function(e) {
+            if ($(e.target).closest("#main-menu").length == 0 && $(e.target).closest("#open").length == 0) {
+                $('#main-menu').stop().slideUp()
+
+            }
+        })
+        // 介绍折叠卡
+    $('.y_hive').css("display", "none")
+    $('.y_but_panel').on('click', 'span', function() {
+        $(this).parent().siblings().stop().slideToggle().
+        parent('.panel').siblings().find('.panel-body').slideUp()
+
+    })
+});
